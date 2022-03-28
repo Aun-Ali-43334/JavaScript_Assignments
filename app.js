8c141ffab0003fcb11a892796ea6ad6b6e3238af
aun.43334@iqra.edu.pk <aun.43334@iqra.edu.pk>
2022-03-28 23:03:18 +0500

    First Commit

---
+++ app.js
@@ -0,0 +1,94 @@
+// -------Chapter No 1-------//
+// Alert
+
+
+// 1. Write a script to greet your website visitor using JS alert box.
+// Solution: 
+// alert("Hello How are You ?")
+
+// 2. Write a script to display following message on your web page:
+// Solution: 
+// alert("Eror! Please Enter Valid Password.")
+
+
+// 3. Write a script to display following message on your web page: (Hint : Use line break)
+// Solution: 
+// alert("Welcome to JS Land \nHappy Coding!")
+
+
+// 4. Write a script to display following messages in sequence:
+// Solution: 
+// alert("Welcome to JS Land")
+// alert("Happy Coding")
+
+// 5. Generate the following message through browser’s developer console:
+// Solution: 
+// Browser Written Code in Console
+// alert("\nHello I can run Js throught my Broswer Windows");
+
+
+
+
+
+// Chapter No 2
+// VARIABLES FOR STRINGS
+// 1. Declare a variable called username.
+var userName ;
+
+
+// 2. Declare a variable called myName & assign to it a string that represents your Full Name.
+// solution:
+var myName = "Aun Ali";
+
+
+
+// 3. Write script to
+// a) Declare a JS variable, titled message.
+// b) Assign “Hello World” to variable message
+// c) Display the message in alert box
+// solution:
+
+// var message ; 
+// message = "Hello World";
+// alert(message);
+
+
+
+// 4. Write a script to save student’s bio data in JS variables and show the data in alert boxes.
+// solution:
+
+// var studentName1 = "Aun Ali";
+// var studentAge = 15;
+// var studentCourse = "Web Mobile Application Development";
+// alert("First Student Name : " + " "+ studentName1);
+// alert("Student Age : " + " "+ studentAge);
+// alert("Student Course : " + " "+ studentCourse);
+
+
+
+// 5. Write a script to display the following alert using one JS variable:
+// solution:
+// alert("PIZZA\nPIZZ\nPIZ\nPI\nP")
+
+
+
+// 6. Declare a variable called email and assign to it a string that represents your Email Address(e.g. example@example.com). Show the blow mentioned message in an alert box.(Hint: use string concatenation)
+// solution:
+
+// var email = "aun.43334@iqra.edu.pk";
+// alert("Email Address Value : "+ email);
+
+
+// 7. Declare a variable called book & give it the value “A smarter way to learn JavaScript”. Display the following message in an alert box:
+// solution:
+
+
+
+
+
+
+
+
+
+
+

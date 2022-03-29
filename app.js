0bf21c9fdc51618065b2b7a54fb1e82810dc2ee6
Aun-Ali-43334 <aun.43334@iqra.edu.pk>
2022-03-29 14:59:57 +0500

    chapter 5, Question 11

--- app.js
+++ app.js
@@ -140,6 +140,204 @@
 // var name; var age ; var Class;
 
 
+// 2. Declare 5 legal & 5 illegal variable names
+// solution:
+
+
+// 3. Display this in your browser
+// solution:
+
+// var a= "(a) A heading stating “Rules for naming JS variables";
+// var b= "(b) Variable names can only contain ______, ______,______ and ______.";
+// var c = "c) Variables must begin with a ______, ______ or_____. For example $name, _name or name";
+// var d = "d) Variable names are case _________";
+// var e = "e) Variable names should not be JS _________";
+// document.write("<p>"+a+"<p/>"+"<p>"+b+"<p/>"+"<p>"+d+"<p/>"+"<p>"+e+"<p/>");
+
+
+// Chapter No 5 
+// MATH EXPRESSIONS
+
+// 1. Write a program that take two numbers & add them in a new variable. Show the result in your browser.
+
+// solution
+// var num1 = +prompt("Enter Number 1");
+// var num2 = +prompt("Enter Number 2");
+// var result = num1 + num2;
+// document.write(result); 
+
+// 2. Repeat task1 for subtraction, multiplication, division & modulus
+// solution
+// var num1 = +prompt("Enter Number 1");
+// var num2 = +prompt("Enter Number 2");
+// var addResult = ("Addition : "+ (num1+num2));
+// var subResult = ("Subtraction : "+ (num1-num2));
+// var multiResult = ("Multiplication : "+ (num1*num2));
+// var divResult = ("Division : "+ (num1/num2));
+// var modResult = ("Division : "+ (num1%num2));
+
+// document.write("<p>"+addResult+"<p/>"); 
+// document.write("<p>"+subResult+"<p/>"); 
+// document.write("<p>"+multiResult+"<p/>"); 
+// document.write("<p>"+divResult+"<p/>"); 
+// document.write("<p>"+modResult+"<p/>"); 
+// --------------- OR ---------------------
+// document.write(addResult+"<br/>"); 
+// document.write(subResult+"<br/>"); 
+// document.write(multiResult+"<br/>"); 
+// document.write(divResult+"<br/>"); 
+// document.write(modResult+"<br/>"); 
+
+
+
+// 3. Do the following using JS Mathematic Expressions
+
+// var num;
+// document.write("Value after Declration is : " , num,"<br />");
+// num = 45;
+// document.write("Value after Initialization is : " , num,"<br />");
+// num ++;
+// document.write("Value after increment is : " , num,"<br />");
+// num = num + 7;
+// document.write("Value after addition of 7 is : " , num,"<br />");
+// num --;
+// document.write("Value after Decrement is : " , num,"<br />");
+// num = num % 3;
+// document.write("Remainder of Value : " , num,"<br />");
+
+
+// 4. Cost of one movie ticket is 600 PKR. Write a script to store ticket price in a variable & calculate the cost of buying 5 tickets to a movie. Example output:
+
+// var costPerTicket = 600;
+// document.write("Cost Per Ticket : ",costPerTicket, "<br />");
+// var noOfTicket = 5;
+// document.write("No Of Ticket : ",noOfTicket, "<br />");
+// var priceOfTicket = costPerTicket*noOfTicket;
+// document.write("Price Of Ticket : ",priceOfTicket, " PKR<br />");
+
+
+// 5. Write a script to display multiplication table of any number in your browser.
+
+// var num = 4; //Table of 4
+// document.write("Table of 4 ","<br />")
+// document.write("4 x 1 = ",num*1,"<br />")
+// document.write("4 x 2 = ",num*2,"<br />")
+// document.write("4 x 3 = ",num*3,"<br />")
+// document.write("4 x 4 = ",num*4,"<br />")
+// document.write("4 x 5 = ",num*5,"<br />")
+// document.write("4 x 6 = ",num*6,"<br />")
+// document.write("4 x 7 = ",num*7,"<br />")
+// document.write("4 x 8 = ",num*8,"<br />")
+// document.write("4 x 9 = ",num*9,"<br />")
+// document.write("4 x 10 = ",num*10,"<br />")
+
+
+// 6. The Temperature Converter: It’s hot out! Let’s make a
+// converter based on the steps here.
+// a. Store a Celsius temperature into a variable.
+// b. Convert it to Fahrenheit & output “NNoC is NNoF”.
+// c. Now store a Fahrenheit temperature into a variable.
+// d. Convert it to Celsius & output “NNoF is NNoC”.
+
+
+// solution:
+
+// var celsiusTemp = 20;
+// document.write("Temp in  Celcius : ",celsiusTemp,"<br />");
+// var farTemp = (celsiusTemp * (9/5)) + 32;
+// document.write("Temp in  Fahrenheit after convert  : ",farTemp,"<br />");
+
+// var farTemp2 = 80;
+// document.write("Temp in  Fahrenheit : ",farTemp2,"<br />")
+// var celsiusTemp2 =  (farTemp2-32)*(5/9);
+// document.write("Temp in  Celsius after convert : ",celsiusTemp2,"<br />"    );
+
+
+
+
+// 7. Write a program to implement checkout process of a shopping cart system for an e-commerce website. Store the following in variables
+// solution
+
+// var priceOfItem1 = 650 ;
+// var quanityOfItem1 = 3 ; 
+// var priceOfItem2 =  100;
+// var quanityOfItem2 = 7 ; 
+
+
+// 8. Store total marks & marks obtained by a student in 2 variables. Compute the percentage & show the result in your browser
+// solution
+// var obtainMarks= 804;
+// document.write("Obtained Marks : "+obtainMarks+ "<br />")
+// var totalMarks= 980;
+// document.write("Total Marks : "+totalMarks+ "<br />")
+// var percentage = (obtainMarks/totalMarks)*100;
+// document.write("Percentage  : "+ percentage+" %" +"<br />")
+
+
+// 9. Assume we have 10 US dollars & 25 Saudi Riyals. Write a script to convert the total currency to Pakistani Rupees. Perform all calculations in a single expression. (Exchange rates : 1 US Dollar = 104.80 Pakistani Rupee and   Saudi Riyal = 28 Pakistani Rupee)
+
+// solution
+// var dollar = 104.80;
+// var reyal = 28;
+// var ans = (dollar*10)+(25*28);
+// document.write("Total Pakistani Currency : " , ans)
+
+
+
+// 10. Write a program to initialize a variable with some number and do arithmetic in following sequence: a. Add 5 b. Multiply by 10 c. Divide the result by 2 Perform all calculations in a single expression
+// solution
+ var number = 25;
+ document.write("Number : ",number,"<br />")
+ document.write("ADD 5 = ",number = number+5,"<br />")
+ document.write("Multiply by 10 = ",number= (number*10) ,"<br />")
+ document.write("Divid by 2 = ",number= (number/2) ,"<br />")
+
+//  11. The Age Calculator: Forgot how old someone is? Calculate it!
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 

5278ef75a3e66deba0eef4cdfa01f5a521bc3e2e
Aun-Ali-43334 <aun.43334@iqra.edu.pk>
2022-03-30 23:10:52 +0500

    Chapter 9 onwards start

--- app.js
+++ app.js
@@ -453,14 +453,31 @@
 // Chapter No 9 to 11
 // USER INPUT & CONDITIONAL STATEMENT 
 
+// 1. Write a program to take “city” name as input from user. If
+// user enters “Karachi”, welcome the user like this:
+// “Welcome to city of lights”
 
+// var input1 = prompt("Enter Your City");
+// if(input1 == "Karachi" || input1 == "karachi" || input1 == "KARACHI" ){
+//     document.write("Welcome to City of Lights");
+// }
 
+// 2. Write a program to take “gender” as input from user. If the
+// user is male, give the message: Good Morning Sir. If the
+// // user is female, give the message: Good Morning Ma’am.
 
+// var inputGender = prompt("Enter Your Gender");
+// if(inputGender == "Male" || inputGender == "male" || inputGender == "MALE"){
+//     document.write("Welcome Sir");
+// }
+// else if(inputGender == "Female" || inputGender == "female" || inputGender == "FEMALE"){
+//     document.write("Welcome Madam");
+// }
 
+// else{
+//     document.write("Please Enter a Valid value");
 
-
-
-
+// }
 
 
 
